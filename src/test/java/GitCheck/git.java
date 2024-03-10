7cf75b4dbd151b1a5b0e42c545d85dcbad7692f1
Manojmj192 <n.manojmj1997@gmail.com>
2024-03-10 14:18:16 +0530

    adding new class

---
+++ src/test/java/GitCheck/git.java
@@ -0,0 +1,30 @@
+package GitCheck;
+
+public class git {
+
+	
+	static String value = "Manoj";
+	
+	
+	public static void df() {
+		
+		
+		System.out.println("i am " + value);
+
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		df();
+		
+		
+		
+	}
+	
+	
+	
+}
